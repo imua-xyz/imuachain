a96810d6f0434b98dc4051561de4a9c0f8be4ade
devin <trestin_blockchain@hotmail.com>
2025-03-17 15:37:25 +0800

    implement gas calculation logic

--- precompiles/bls/bls.go
+++ precompiles/bls/bls.go
@@ -4,8 +4,13 @@ import (
 	"bytes"
 	"embed"
 	"fmt"
+	"math/big"
+
+	"golang.org/x/crypto/sha3"
 
 	"github.com/cometbft/cometbft/libs/log"
+	imuacmn "github.com/imua-xyz/imuachain/precompiles/common"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -56,9 +61,84 @@ func (p Precompile) Address() common.Address {
 	return common.HexToAddress("0x0000000000000000000000000000000000000809")
 }
 
+// Function Selectors
+var (
+	verifySelector              = generateSelector("verify(bytes32,bytes,bytes)")                // verify(bytes32,bytes,bytes)
+	fastAggregateVerifySelector = generateSelector("fastAggregateVerify(bytes32,bytes,bytes[])") // fastAggregateVerify(bytes32,bytes,bytes[])
+	aggregatePubKeysSelector    = generateSelector("aggregatePubKeys(bytes[])")                  // aggregatePubKeys(bytes[])
+	aggregateSignaturesSelector = generateSelector("aggregateSignatures(bytes[])")               // aggregateSignatures(bytes[])
+	addTwoPubKeysSelector       = generateSelector("addTwoPubKeys(bytes,bytes)")                 // addTwoPubKeys(bytes,bytes)
+)
+
 // RequiredGas calculates the precompiled contract's base gas rate.
-func (p Precompile) RequiredGas(_ []byte) uint64 {
-	return p.baseGas
+func (p Precompile) RequiredGas(input []byte) uint64 {
+	if len(input) < 4 {
+		return 0
+	}
+	selector := input[:4]
+	switch {
+	case bytes.Equal(selector, verifySelector):
+		return imuacmn.Bls12381PairingBaseGas + imuacmn.Bls12381PairingPerPairGas
+
+	case bytes.Equal(selector, fastAggregateVerifySelector):
+		return p.calculateFastAggregateVerifyGas(input)
+
+	case bytes.Equal(selector, aggregatePubKeysSelector):
+		return p.calculateAggregationGas(input, imuacmn.Bls12381G1AddGas)
+
+	case bytes.Equal(selector, aggregateSignaturesSelector):
+		return p.calculateAggregationGas(input, imuacmn.Bls12381G2AddGas)
+
+	case bytes.Equal(selector, addTwoPubKeysSelector):
+		return imuacmn.Bls12381G1AddGas
+
+	default:
+		return 0
+	}
+}
+
+// FastAggregateVerify gas calculation
+func (p Precompile) calculateFastAggregateVerifyGas(input []byte) uint64 {
+	if len(input) < 4+96 {
+		return 0
+	}
+
+	pubKeysOffset := new(big.Int).SetBytes(input[4+64 : 4+96]).Uint64()
+	if len(input) < int(4+pubKeysOffset+32) {
+		return 0
+	}
+
+	m := new(big.Int).SetBytes(input[4+pubKeysOffset : 4+pubKeysOffset+32]).Uint64()
+	if m < 1 {
+		return 0
+	}
+
+	return (m-1)*imuacmn.Bls12381G1AddGas + (imuacmn.Bls12381PairingBaseGas + imuacmn.Bls12381PairingPerPairGas)
+}
+
+// Generic aggregation gas calculation
+func (p Precompile) calculateAggregationGas(input []byte, gasPerOp uint64) uint64 {
+	if len(input) < 4+32 {
+		return 0
+	}
+
+	offset := new(big.Int).SetBytes(input[4 : 4+32]).Uint64()
+	if len(input) < int(4+offset+32) {
+		return 0
+	}
+
+	n := new(big.Int).SetBytes(input[4+offset : 4+offset+32]).Uint64()
+	if n < 1 {
+		return 0
+	}
+
+	return (n - 1) * gasPerOp
+}
+
+func generateSelector(funcSig string) []byte {
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write([]byte(funcSig))
+	return hash.Sum(nil)[:4]
 }
 
 // Run executes the precompiled contract deposit methods defined in the ABI.
@@ -82,12 +162,12 @@ func (p Precompile) Run(_ *vm.EVM, contract *vm.Contract, _ bool) (bz []byte, er
 		bz, err = p.Verify(method, args)
 	case MethodFastAggregateVerify:
 		bz, err = p.FastAggregateVerify(method, args)
-	case MethodAggregatePubkeys:
-		bz, err = p.AggregatePubkeys(method, args)
+	case MethodAggregatePubKeys:
+		bz, err = p.AggregatePubKeys(method, args)
 	case MethodAggregateSignatures:
 		bz, err = p.AggregateSignatures(method, args)
-	case MethodAddTwoPubkeys:
-		bz, err = p.AddTwoPubkeys(method, args)
+	case MethodAddTwoPubKeys:
+		bz, err = p.AddTwoPubKeys(method, args)
 	default:
 		return nil, fmt.Errorf("invalid method")
 	}
@@ -104,16 +184,7 @@ func (p Precompile) Run(_ *vm.EVM, contract *vm.Contract, _ bool) (bz []byte, er
 // Available bls transactions are:
 //   - MethodVerify
 func (Precompile) IsTransaction(methodID string) bool {
-	switch methodID {
-	case MethodVerify,
-		MethodFastAggregateVerify,
-		MethodAggregatePubkeys,
-		MethodAggregateSignatures,
-		MethodAddTwoPubkeys:
-		return true
-	default:
-		return false
-	}
+	return false
 }
 
 // Logger returns a precompile-specific logger.
