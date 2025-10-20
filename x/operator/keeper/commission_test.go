9e9a4f7dcabec639513c7791c46688ada51d078d
TimmyExogenous <chuang@exogenous.io>
2025-10-20 16:42:07 +0800

    fix the failed unit tests after merging

--- x/operator/keeper/commission_test.go
+++ x/operator/keeper/commission_test.go
@@ -31,7 +31,7 @@ func (suite *CommissionTestSuite) TestCommissionRateTimeBound() {
 			MaxRate:       sdk.OneDec(),
 			MaxChangeRate: sdk.OneDec(),
 		},
-	})
+	}, true)
 	suite.Commit()
 	// change to 6% immediately
 	targetCommissionRate := sdk.NewDecWithPrec(6, 2)
@@ -78,7 +78,7 @@ func (suite *CommissionTestSuite) TestCommissionRateChange() {
 			MaxRate:       sdk.NewDecWithPrec(75, 2),
 			MaxChangeRate: sdk.NewDecWithPrec(5, 2),
 		},
-	})
+	}, true)
 	duration := suite.App.OperatorKeeper.GetMinCommissionUpdateInterval(suite.Ctx)
 	// from 0 we can go to 5% but not higher
 	// let's try 5.1%
