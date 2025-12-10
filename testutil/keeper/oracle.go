6363d418820aeadf7ebf339e5c87d6e9048dd481
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2025-12-10 09:52:23 +0000

    fix: tests should use correct addr

--- testutil/keeper/oracle.go
+++ testutil/keeper/oracle.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -12,7 +13,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/address"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/group"
+	groupkeeper "github.com/cosmos/cosmos-sdk/x/group/keeper"
 	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/imua-xyz/imuachain/cmd/config"
 	"github.com/imua-xyz/imuachain/x/oracle/keeper"
@@ -26,9 +29,17 @@ import (
 )
 
 func GetAuthAddrString() string {
-	authAddrBz := address.Module("cosmos.group.v1.GroupPolicy", []byte("1"))
-	authAddr := sdk.AccAddress(authAddrBz)
-	return authAddr.String()
+	cred, err := authtypes.NewModuleCredential(
+		group.ModuleName,
+		[]byte{groupkeeper.GroupPolicyTablePrefix},
+		sdk.Uint64ToBigEndian(1),
+	)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create module credential: %s", err))
+	}
+	authAddr := sdk.AccAddress(cred.Address().Bytes())
+	authAddrString := authAddr.String()
+	return authAddrString
 }
 
 func OracleKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
