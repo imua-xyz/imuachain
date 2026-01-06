9d69ccfbc4adf5454fcb5d9f1966164004867a4d
Max <82761650+MaxMustermann2@users.noreply.github.com>
2026-01-06 15:32:05 +0530

    feat: temporary multi-sig gov with x/group (#396)
    * fix: permit 2 letter denoms in SDK
    
    * fix(local): allow run
    
    1. Change min commission and other params
    2. Complain if BOOTSTRAP is not set
    
    * fix(app): use group 1 as authority
    
    * fix(imua-mod): add back authority check
    
    * fix(local): create default group
    
    * fix: tests
    
    * remove duplicate code
    
    * fix: use correct x/group address
    
    * fix: tests should use correct addr
    
    * fix(app): update comment per AI
    
    * chore(lint): gofumpt
    
    ---------
    
    Co-authored-by: cloud8little <34291844+cloud8little@users.noreply.github.com>
    Co-authored-by: Mike <142298428+mikebraver@users.noreply.github.com>

--- x/assets/keeper/msg_server.go
+++ x/assets/keeper/msg_server.go
@@ -5,7 +5,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	"github.com/imua-xyz/imuachain/utils"
 	assetstype "github.com/imua-xyz/imuachain/x/assets/types"
 )
 
@@ -14,7 +13,7 @@ var _ assetstype.MsgServer = &Keeper{}
 // UpdateParams This function should be triggered by the governance in the future
 func (k Keeper) UpdateParams(ctx context.Context, params *assetstype.MsgUpdateParams) (*assetstype.MsgUpdateParamsResponse, error) {
 	c := sdk.UnwrapSDKContext(ctx)
-	if utils.IsMainnet(c.ChainID()) && k.authority != params.Authority {
+	if k.authority != params.Authority {
 		return nil, govtypes.ErrInvalidSigner.Wrapf(
 			"invalid authority; expected %s, got %s",
 			k.authority, params.Authority,
